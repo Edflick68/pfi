05ece06040269f39457204cf1173e4352eaee553
Edflick68 <edstm88@gmail.com>
2026-04-26 22:29:56 -0400

    Database commencé

--- app/connexion.js
+++ app/connexion.js
@@ -1,1 +1,11 @@
 import { View, TextInput,  } from "react-native";
+import { useSQLiteContext, SQLiteProvider } from "expo-sqlite";
+
+async function initDB(db) {
+  await db.execAsync(`DROP TABLE IF EXISTS client`);
+  await db.execAsync(`PRAGMA journal_mode = WAL;
+    CREATE TABLE IF NOT EXISTS client (nom TEXT, mdp TEXT, admin BOOL, adresse TEXT, langue TEXT);
+    INSERT INTO client(nom, mdp, admin, adresse, langue) values('Lina', 'password', false, '54 Rue Trollet', 'Français');
+    INSERT INTO client(nom, mdp, admin, adresse, langue) values('Edouard', 'password', true, '6 Rue Drollet', 'Anglais');
+    INSERT INTO client(nom, mdp, admin, adresse, langue) values('Sean', 'password', false, '90 Rue Jean-Dallaire', 'Français');`);
+}
--- app/pagesAdmin/listeProduit.js
+++ app/pagesAdmin/listeProduit.js
@@ -1,1 +1,21 @@
 import { FlatList, Text, Pressable, StyleSheet } from "react-native";
+import { useSQLiteContext, SQLiteProvider } from "expo-sqlite";
+async function initDB(db) {
+  await db.execAsync(`DROP TABLE IF EXISTS produit`);
+  await db.execAsync(`PRAGMA journal_mode = WAL;
+    CREATE TABLE IF NOT EXISTS produit (id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nom TEXT, description TEXT, prix TEXT, image TEXT);
+    INSERT INTO client(nom, description, prix, image) values('Champignon');
+    INSERT INTO client(nom, description, prix, image) values('Étoile');
+    INSERT INTO client(nom, description, prix, image) values('Haut-Parleur');
+    INSERT INTO client(nom, description, prix, image) values('Fusée');
+    INSERT INTO client(nom, description, prix, image) values('Fleur de feu');
+    INSERT INTO client(nom, description, prix, image) values('Fleur Boomerang');
+    INSERT INTO client(nom, description, prix, image) values('Banane');
+    INSERT INTO client(nom, description, prix, image) values('Éclair');
+    INSERT INTO client(nom, description, prix, image) values('Champignon doré');
+    INSERT INTO client(nom, description, prix, image) values('Carapace verte');
+    INSERT INTO client(nom, description, prix, image) values('Carapace rouge');
+    INSERT INTO client(nom, description, prix, image) values('Carapace épineuse');
+    INSERT INTO client(nom, description, prix, image) values('Bombe');`);
+}
