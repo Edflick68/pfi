d6aa486f6c7eefe59f92771cddfe3de886c80b21
Edflick68 <edstm88@gmail.com>
2026-04-30 21:51:57 -0400

    Update index.js

--- app/index.js
+++ app/index.js
@@ -5,7 +5,7 @@ async function initDB(db) {
   await db.execAsync(`DROP TABLE IF EXISTS client`);
   await db.execAsync(`PRAGMA journal_mode = WAL;
     CREATE TABLE IF NOT EXISTS client (nom TEXT, mdp TEXT, admin BOOL, adresse TEXT, langue TEXT);
-    INSERT INTO client(nom, mdp, admin, adresse, langue) values('Lina', 'password', false, '54 Rue Trollet', 'Français');
-    INSERT INTO client(nom, mdp, admin, adresse, langue) values('Edouard', 'password', true, '6 Rue Drollet', 'Anglais');
-    INSERT INTO client(nom, mdp, admin, adresse, langue) values('Sean', 'password', false, '90 Rue Jean-Dallaire', 'Français');`);
+    INSERT INTO client(nom, mdp, admin, adresse, langue) values('Lina', 'password', false, '54 Rue Trollet', 'fr');
+    INSERT INTO client(nom, mdp, admin, adresse, langue) values('Edouard', 'password', true, '6 Rue Drollet', 'en');
+    INSERT INTO client(nom, mdp, admin, adresse, langue) values('Sean', 'password', true, '90 Rue Jean-Dallaire', 'fr');`);
 }
