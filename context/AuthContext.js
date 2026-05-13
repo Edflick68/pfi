89861b78536cbe1c2c2479e5b2c2f64634ab24db
SeanEDubois <seanedwarddubois@gmail.com>
2026-05-13 17:20:45 -0400

    authcontext

---
+++ context/AuthContext.js
@@ -0,0 +1,47 @@
+import { createContext, useContext, useState } from "react";
+import { useSQLiteContext } from "expo-sqlite";
+import { useTranslation } from "react-i18next";
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const db = useSQLiteContext();
+
+  const { i18n } = useTranslation();
+
+  const login = async (nom, mdp) => {
+    try {
+      const result = await db.getFirstAsync(
+        "SELECT * FROM client WHERE nom = ? AND mdp = ?",
+        [nom, mdp],
+      );
+
+      if (result) {
+        setUser(result);
+
+        if (result.langue) {
+          i18n.changeLanguage(result.langue);
+        }
+
+        return true;
+      }
+      return false;
+    } catch (error) {
+      console.error("Erreur de connexion:", error);
+      return false;
+    }
+  };
+
+  const logout = () => {
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export const useAuth = () => useContext(AuthContext);
