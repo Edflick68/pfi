e0fbed9df82e84c33b1337cecf92c7d31ef0c8c9
Edflick68 <edstm88@gmail.com>
2026-05-13 22:51:36 -0400

    Update _layout.js

--- app/(main)/pagesClients/_layout.js
+++ app/(main)/pagesClients/_layout.js
@@ -1,14 +1,16 @@
 import { Tabs } from 'expo-router'
 import Ionicons from '@expo/vector-icons/Ionicons';
+import { useTranslation } from 'react-i18next';
 const TabsLayout = () => {
+    const {t} = useTranslation();
 return (<Tabs
 screenOptions={{headerShown:false, tabBarActiveTintColor:"blue", tabBarInactiveTintColor: "#87CEFA"}}>
 <Tabs.Screen name="produits" 
-options={{title: "Produits", tabBarIcon: ({color, focused})=> <Ionicons name="list" size={24} color={color}/>}} />
+options={{title: t("products"), tabBarIcon: ({color, focused})=> <Ionicons name="list" size={24} color={color}/>}} />
 <Tabs.Screen name="panier" 
-options={{title: "Panier",tabBarIcon: ({color, focused})=> <Ionicons name="cart" size={24} color={color}/> }} />
+options={{title: t("cart"),tabBarIcon: ({color, focused})=> <Ionicons name="cart" size={24} color={color}/> }} />
 <Tabs.Screen name="compte" 
-options={{title: "Compte",tabBarIcon: ({color, focused})=> <Ionicons name="person" size={24} color={color}/>}}/>
+options={{title: t("account"),tabBarIcon: ({color, focused})=> <Ionicons name="person" size={24} color={color}/>}}/>
 </Tabs>)
 }
 export default TabsLayout
