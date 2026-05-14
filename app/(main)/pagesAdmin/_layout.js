2c6b4a6d1586817f0c6ae05ad62ca0b808597c03
Edflick68 <edstm88@gmail.com>
2026-05-13 20:06:25 -0400

    Update

--- app/(main)/pagesAdmin/_layout.js
+++ app/(main)/pagesAdmin/_layout.js
@@ -1,6 +1,7 @@
 import {Stack, router} from "expo-router";
 import { useSQLiteContext } from "expo-sqlite";
 import {useEffect, useState} from "react";
+import i18n from "../../../i18n";
 
 export default function AdminLayout(){
     const db = useSQLiteContext();
@@ -8,11 +9,11 @@ export default function AdminLayout(){
         <Stack>
             <Stack.Screen
             name = "listeProduit"
-            options ={{title: "Liste de Produits et suppression"}}/>
+            options ={{title: i18n.t("admin_products")}}/>
 
             <Stack.Screen
             name = "ajouter"
-            options = {{title: "Ajouter un produit"}}/>
+            options = {{title: i18n.t("add_product")}}/>
         </Stack>
     );
 }
